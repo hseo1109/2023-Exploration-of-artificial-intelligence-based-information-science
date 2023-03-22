61fa29a3ee7da150601acc9b2401835cfa192e7a
hseo1109 <127501925+hseo1109@users.noreply.github.com>
2023-03-22 20:09:56 +0900

    week03 Temperature_Converter v0.3

--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -1,15 +1,15 @@
 while True:
-    menu=int(input("1) fahrenheit to celsius   2) celcius to fahrenheit   3)exit :"))
+    menu=input("1) fahrenheit to celsius   2) celcius to fahrenheit   3)exit :")
 
-    if menu==1:
+    if menu=="1":
         fahrenheit=float(input('Enter temperature in Fahrenheit : '))
         celsius=(fahrenheit-32.0)*(5.0/9.0)
         print(f"{fahrenheit:.2f} degrees Fahrenheit is {celsius:.2f} degrees Celsius.")
-    elif menu==2:
+    elif menu=="2":
         fahrenheit = float(input('Enter temperature in Celcius : '))
         fahrenheit=(celsius*9.0/5.0)+32.0
         print(f"{fahrenheit:.2f} degrees Celsius is {celsius:.2f} degrees Fahrenheit")
-    elif menu==3:
+    elif menu=="3":
         break
     else:
         print("Please choose from the given menu.")
