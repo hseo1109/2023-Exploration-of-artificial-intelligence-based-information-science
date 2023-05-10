cc896665554ac29731be7a812b8dbacc54aa1750
hseo1109 <127501925+hseo1109@users.noreply.github.com>
2023-05-10 19:17:39 +0900

    ãISHS test

--- week06_function.py
+++ week06_function.py
@@ -12,10 +12,10 @@ def time_checker(func): #closure
         return r
     return inner
 
-def factorial(n):
+def factorial_repetition(n):
     """
     factorial function
-    :param n: more than zero
+    :param n: > 0
     :return: n!
     """
     result=1
@@ -23,20 +23,39 @@ def factorial(n):
         result=result*k
     return result
 
-@time_checker
-def power(b,e):
-    """
-    power function
-    :param b: base ex) 2
-    :param e: exponent ex) 10
-    :return: ex) 2 to the 10th power is 10th
-    """
-    result=1
-    for _ in range(e):
-        result=result*b
-    return result
 
-print(power(2,10))
+def factorial_recursion(n):
+    """
+    factorial function with recursion
+    :param n: > 0
+    :return: n!
+    """
+    if n==1:
+        return 1
+    else:
+        return n*factorial_recursion(n-1)
 
-df=time_checker(factorial)
-print(df(1000))
+def fibonacci_recursion(n):
+    """
+    fibonacci number recursion
+    :param n: integer value
+    :return: fibonacci number
+    """
+    if n<=1:
+        return n
+    else:
+        return fibonacci_recursion(n-1)+fibonacci_recursion(n-2)
+def fibonacci_repetition(n):
+    a=[0,1]
+    for i in range(2,n+1):
+        a.append(a[i-1]+a[i-2])
+    return a[n]
+
+if __name__=="__main__":
+    number=int(input("정수 입력: "))
+    print(factorial_repetition(number))
+    print(factorial_recursion(number))
+    print(fibonacci_repetition(number))
+    print(fibonacci_recursion(number))
+
+
---
+++ week07.py
@@ -0,0 +1,8 @@
+import my_util
+
+@my_util.time_checker
+def factorial_repetition(n):
+@my_util.time_checker
+def factorial_repetition(n):
+@my_util.time_checker
+def factorial_repetition(n):
