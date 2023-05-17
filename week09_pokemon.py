c7a013081989d4a8a6dff26df054f93e7474588c
hseo1109 <127501925+hseo1109@users.noreply.github.com>
2023-05-17 21:50:44 +0900

    week09_class_object02

--- week09_pokemon.py
+++ week09_pokemon.py
@@ -1,13 +1,26 @@
 class Pokemon:
+    def __init__(self,name,level,hp):
+        self.name = name
+        self.level = level
+        self.hp = hp
+    pass
+    def attack(self):
+        print(f"{self,name} launches an area attack")
+    def attack_target(self,target):
+        print(f"{self,name} attacks {target}!")
+
+class Pikachu(Pokemon): #is a relationship
+    pass
+class Squirtle(Pokemon):
     pass
 
 if __name__ == "__main__":
-    pikachu = Pokemon() #instance creation
-    eevee = Pokemon() #incstance creation
-    pikachu.level = 1
-    pikachu.hp = 35
-    pikachu.nemesis = eevee
+    pikachu = Pikachu("pikachu",1,35)
+    squirtle = Squirtle("squirtle",1,44)
+    charizard = Pokemon("charizard",36,78)
 
     print(pikachu.hp)
-    print(pikachu.nemesis)
+    print(pikachu.level)
+    print(pikachu.name)
+    print(pikachu)
 
